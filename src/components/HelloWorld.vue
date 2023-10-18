bc4dbf441915d0ad5a1041de6a728085ba54213f
ayres01 <139259824+ayres01@users.noreply.github.com>
2023-10-18 10:29:55 +0100

    Update HelloWorld.vue
    change API endpoint URL

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -26,7 +26,7 @@ const MAX_IMAGE_SIZE = 1000000
 /* ENTER YOUR ENDPOINT HERE
    FILES UPLOADED TO MY ENDPOINT ARE AUTOMATICALLY DELETED EVERY FEW HOURS */
 
-const API_ENDPOINT = ' https://adzqusfoof.execute-api.eu-north-1.amazonaws.com/default/funmiserverlesslambda'
+const API_ENDPOINT = 'https://qmooz94be6.execute-api.eu-north-1.amazonaws.com/default/funmi-serverless-lambda'
 
 export default {
   name: 'app',
